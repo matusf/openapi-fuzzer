460922fe78afe47e9fd7f2e17884d717d5c6edd2
Matúš Ferech <matus.ferech@gmail.com>
2023-01-24 23:15:07 +0100

    Implement fuzzer payload generation using arbitrary types

--- src/fuzzer.rs
+++ src/fuzzer.rs
@@ -1,75 +1,29 @@
 use std::{
-    collections::BTreeMap,
+    collections::HashMap,
     fs::{self, File},
-    u32,
+    rc::Rc,
 };
 
-use anyhow::{Context, Result};
+use anyhow::Result;
+use indexmap::IndexMap;
 use openapi_utils::ReferenceOrExt;
-use openapiv3::{OpenAPI, StatusCode};
+use openapiv3::{OpenAPI, ReferenceOr, Response, StatusCode};
+use proptest::{
+    prelude::any_with,
+    test_runner::{Config, FileFailurePersistence, TestCaseError, TestError, TestRunner},
+};
 use serde_json::json;
 use ureq::OrAnyStatus;
 use url::Url;
 
-use crate::request::Request;
-use crate::tui::Tui;
-
-#[derive(Debug, Default)]
-pub struct Tries {
-    pub total: u32,
-    pub successful: u32,
-}
-
-impl Tries {
-    fn update(&mut self, success: bool) {
-        self.total += 1;
-        if success {
-            self.successful += 1;
-        }
-    }
-}
-#[derive(Debug, Default)]
-pub struct Stats {
-    pub ignored_status_codes: Vec<u16>,
-    pub frequencies: BTreeMap<String, BTreeMap<String, Tries>>,
-    pub total: u32,
-}
-
-impl Stats {
-    fn new(ignored_status_codes: Vec<u16>) -> Stats {
-        Stats {
-            ignored_status_codes,
-            total: 0,
-            ..Default::default()
-        }
-    }
-
-    fn update(&mut self, resp: &ureq::Response, payload: &Request) {
-        self.total += 1;
-        let success = !(self.ignored_status_codes.contains(&resp.status())
-            || (payload
-                .responses
-                .responses
-                .contains_key(&StatusCode::Code(resp.status()))
-                && resp.status() / 100 != 5));
-
-        self.frequencies
-            .entry(payload.path.to_string())
-            .or_insert_with(BTreeMap::new)
-            .entry(payload.method.to_string())
-            .or_insert_with(Tries::default)
-            .update(success);
-    }
-}
+use crate::arbitrary::{ArbitraryParameters, Payload};
 
 #[derive(Debug)]
 pub struct Fuzzer {
     schema: OpenAPI,
     url: Url,
     ignored_status_codes: Vec<u16>,
-    extra_headers: Vec<(String, String)>,
-    stats: Stats,
-    tui: Tui,
+    extra_headers: HashMap<String, String>,
 }
 
 impl Fuzzer {
@@ -77,94 +31,180 @@ impl Fuzzer {
         schema: OpenAPI,
         url: Url,
         ignored_status_codes: Vec<u16>,
-        extra_headers: Vec<(String, String)>,
+        extra_headers: HashMap<String, String>,
     ) -> Fuzzer {
         Fuzzer {
             schema,
             url,
             extra_headers,
-            ignored_status_codes: ignored_status_codes.clone(),
-            stats: Stats::new(ignored_status_codes),
-            tui: Tui::new().expect("unable to setup tui"),
+            ignored_status_codes,
         }
     }
 
-    pub fn run(&mut self) -> Result<()> {
-        let mut message = None;
-        loop {
-            for (path, ref_or_item) in self.schema.paths.iter() {
-                let item = ref_or_item.to_item_ref();
-                for payload in Request::for_all_methods(&self.url, path, item, &self.extra_headers)?
-                {
-                    match self.send_request(&payload) {
-                        Ok(resp) => {
-                            self.check_response(&resp, &payload)?;
-                            self.stats.update(&resp, &payload);
-                            message = None;
+    pub fn run(&mut self) {
+        let config = Config {
+            failure_persistence: Some(Box::new(FileFailurePersistence::Direct(
+                "openapi-fuzzer.regressions",
+            ))),
+            verbose: 0,
+            ..Config::default()
+        };
+        let mut runner = TestRunner::new(config);
+        let max_path_length = self.schema.paths.iter().map(|(path, _)| path.len()).max();
+
+        for (path_with_params, ref_or_item) in self.schema.paths.iter() {
+            let path_with_params = path_with_params.trim_start_matches('/');
+            let item = ref_or_item.to_item_ref();
+            let operations = vec![
+                ("GET", &item.get),
+                ("PUT", &item.put),
+                ("POST", &item.post),
+                ("DELETE", &item.delete),
+                ("OPTIONS", &item.options),
+                ("HEAD", &item.head),
+                ("PATCH", &item.patch),
+                ("TRACE", &item.trace),
+            ];
+
+            for (method, op) in operations {
+                if let Some(operation) = op {
+                    let result = runner.run(
+                        &any_with::<Payload>(Rc::new(ArbitraryParameters::new(Box::new(
+                            operation.clone(),
+                        )))),
+                        |payload| {
+                            let response = match Fuzzer::send_request(
+                                &self.url,
+                                path_with_params.to_owned(),
+                                method,
+                                &payload,
+                                &self.extra_headers,
+                            ) {
+                                Ok(response) => response,
+                                Err(e) => {
+                                    return Err(TestCaseError::Fail(
+                                        format!("unable to send request: {}", e).into(),
+                                    ))
+                                }
+                            };
+
+                            match self
+                                .is_expected_response(&response, &operation.responses.responses)
+                            {
+                                true => Ok(()),
+                                false => {
+                                    Fuzzer::save_finding(
+                                        path_with_params,
+                                        method,
+                                        &payload,
+                                        &response,
+                                    )?;
+                                    Err(TestCaseError::Fail(
+                                        format!("{}", response.status()).into(),
+                                    ))
+                                }
+                            }
+                        },
+                    );
+
+                    match result {
+                        Err(TestError::Fail(_, _)) => {
+                            println!(
+                                "{:7} {:width$} {:^7}",
+                                &method,
+                                &path_with_params,
+                                "failed",
+                                width = max_path_length.unwrap()
+                            );
                         }
-                        Err(e) => message = Some(e.to_string()),
-                    };
+                        Ok(()) => {
+                            println!(
+                                "{:7} {:width$} {:^7}",
+                                &method,
+                                &path_with_params,
+                                "ok",
+                                width = max_path_length.unwrap()
+                            )
+                        }
+                        Err(TestError::Abort(_)) => {
+                            println!(
+                                "{:7} {:width$} {:^7}",
+                                &method,
+                                &path_with_params,
+                                "aborted",
+                                width = max_path_length.unwrap()
+                            )
+                        }
+                    }
                 }
             }
-            if self
-                .tui
-                .display(&self.stats, &message)
-                .context("unable to draw tui")?
-            {
-                return Ok(());
-            }
         }
     }
 
-    fn send_request(&self, payload: &Request) -> Result<ureq::Response> {
-        let mut path_with_params = payload.path.to_owned();
-        for (name, value) in payload.path_params.iter() {
+    fn send_request(
+        url: &Url,
+        mut path_with_params: String,
+        method: &str,
+        payload: &Payload,
+        extra_headers: &HashMap<String, String>,
+    ) -> Result<ureq::Response> {
+        for (name, value) in payload.path_params().iter() {
             path_with_params = path_with_params.replace(&format!("{{{}}}", name), value);
         }
-        let mut request = ureq::request_url(
-            payload.method,
-            &payload.url.join(path_with_params.trim_start_matches('/'))?,
-        );
+        let mut request = ureq::request_url(method, &url.join(&path_with_params)?);
 
-        for (param, value) in payload.query_params.iter() {
+        for (param, value) in payload.query_params().iter() {
             request = request.query(param, value)
         }
 
-        for (header, value) in payload.headers.iter() {
-            request = request.set(header, value)
+        // Add headers overriding genereted ones with extra headers from command line
+        for (header, value) in payload.headers().iter() {
+            let value = extra_headers.get(&header.to_lowercase()).unwrap_or(value);
+            request = request.set(header, value);
         }
 
-        if !payload.body.is_empty() {
-            Ok(request.send_json(payload.body[0].clone()).or_any_status()?)
-        } else {
-            request.call().or_any_status().map_err(|e| e.into())
+        // Add remaining extra headers
+        for (header, value) in extra_headers.iter() {
+            if request.header(header).is_none() {
+                request = request.set(header, value);
+            }
         }
+
+        match payload.body() {
+            Some(json) => request.send_json(json.clone()),
+            None => request.call(),
+        }
+        .or_any_status()
+        .map_err(Into::into)
     }
 
-    fn check_response(&self, resp: &ureq::Response, payload: &Request) -> Result<()> {
-        let responses = &payload.responses.responses;
-
+    fn is_expected_response(
+        &self,
+        resp: &ureq::Response,
+        responses: &IndexMap<StatusCode, ReferenceOr<Response>>,
+    ) -> bool {
         // known non 500 and ingored status codes are OK
-        if self.ignored_status_codes.contains(&resp.status())
+        self.ignored_status_codes.contains(&resp.status())
             || (responses.contains_key(&StatusCode::Code(resp.status()))
                 && resp.status() / 100 != 5)
-        {
-            return Ok(());
-        }
+    }
 
+    fn save_finding(
+        path: &str,
+        method: &str,
+        payload: &Payload,
+        response: &ureq::Response,
+    ) -> std::io::Result<()> {
         let results_dir = format!(
-            "results/{}/{}/{}",
-            payload.path.trim_matches('/').replace('/', "-"),
-            payload.method,
-            resp.status()
+            "openapi-fuzzer-results/{}/{}/{}",
+            path.trim_matches('/').replace('/', "-"),
+            method,
+            response.status()
         );
         let results_file = format!("{}/{:x}.json", results_dir, rand::random::<u32>());
         fs::create_dir_all(&results_dir)?;
 
-        serde_json::to_writer_pretty(
-            &File::create(&results_file).context(format!("unable to create {}", &results_file))?,
-            &json!({"payload": payload, "curl": payload.to_curl()?}),
-        )
-        .map_err(|e| e.into())
+        serde_json::to_writer_pretty(&File::create(results_file)?, &json!({ "payload": payload }))
+            .map_err(|e| e.into())
     }
 }
